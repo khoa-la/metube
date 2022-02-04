58640ea1cdbb8253433a2a5bef3b5a815a0cd55d
khoa-la <lakhoa2806@gmail.com>
2022-02-04 08:47:53 +0700

    2/4

--- index.js
+++ index.js
@@ -76,3 +76,24 @@ document.addEventListener("DOMContentLoaded", function () {
     );
   });
 });
+
+function downloadImage() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", searchInput.value, true);
+  xhr.responseType = "blob";
+  xhr.onload = function () {
+    var urlCreator = window.URL || window.webkitURL;
+    var imageUrl = urlCreator.createObjectURL(this.response);
+    var tag = document.createElement("a");
+    tag.href = imageUrl;
+    tag.target = "_blank";
+    tag.download = "sample.png";
+    document.body.appendChild(tag);
+    tag.click();
+    document.body.removeChild(tag);
+  };
+  xhr.onerror = (err) => {
+    alert("Failed to download picture");
+  };
+  xhr.send();
+}
